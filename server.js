0ed7c521da4b20614052b00bfae99de63809fd9b
mylesrverigin <mylesrverigin@gmail.com>
2021-11-14 12:28:40 -0800

    Adding more test endpoints

--- server.js
+++ server.js
@@ -9,5 +9,20 @@ const PORT = process.env.PORT || 8081
 app.get('/',(req,res)=>{
     res.status(200).send('Hello from the API')
 })
+app.get('/myles',(req,res)=>{
+    res.status(200).send('')
+})
+app.get('/robert',(req,res)=>{
+    res.status(200).send('Robert endpoint')
+})
+app.get('/user',(req,res)=>{
+    let username = Date.now()
+    res.status(200).send('your user is '+username)
+})
+app.get('/data',(req,res)=>{
+    let randomData = Math.random() * Date.now();
+    res.status(200).send('this is important user data ' + randomData);
+})
+
 
 app.listen(PORT, () => { console.log(`Server running on ${PORT}`) })
